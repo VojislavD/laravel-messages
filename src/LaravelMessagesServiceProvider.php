e8b7bc542a72570da64ff4e8ec5f6ae423475ed8
vojislavd <vojislavdragicevic17@gmail.com>
2022-03-12 14:49:26 +0100

    Create service provider

---
+++ src/LaravelMessagesServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace VojislavD\LaravelMessages;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelMessagesServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        
+    }
+
+    public function boot()
+    {
+        
+    }
+}
