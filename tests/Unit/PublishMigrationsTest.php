ebbe08984dfeab1d4846d79abdbfe66364a67147
vojislavd <vojislavdragicevic17@gmail.com>
2022-03-15 09:36:40 +0100

    Test publish migrations when migrations already exists

--- tests/Unit/PublishMigrationsTest.php
+++ tests/Unit/PublishMigrationsTest.php
@@ -2,6 +2,7 @@
 
 namespace VojislavD\LaravelMessages\Tests\Unit;
 
+use Illuminate\Support\Facades\File;
 use VojislavD\LaravelMessages\Tests\TestCase;
 
 class PublishMigrationsTest extends TestCase
@@ -19,6 +20,56 @@ class PublishMigrationsTest extends TestCase
         $this->assertContains('create_threads_table.php', $migrations);
         $this->assertContains('create_thread_participants_table.php', $migrations);
         $this->assertContains('create_messages_table.php', $migrations);
+
+        $content = scandir(database_path('migrations'));
+
+        $this->assertEquals(
+            file_get_contents(__DIR__.'/../../database/migrations/create_threads_table.stub'),
+            file_get_contents(database_path('migrations/'.$content[3]))
+        );
+
+        $this->assertEquals(
+            file_get_contents(__DIR__.'/../../database/migrations/create_thread_participants_table.stub'),
+            file_get_contents(database_path('migrations/'.$content[4]))
+        );
+
+        $this->assertEquals(
+            file_get_contents(__DIR__.'/../../database/migrations/create_messages_table.stub'),
+            file_get_contents(database_path('migrations/'.$content[5]))
+        );
+    }
+
+    /** @test */
+    public function test_publish_migrations_when_already_exists()
+    {
+        $threadsTable = date('Y_m_d_His', time()). '_create_threads_table.php';
+        $threadParticipants = date('Y_m_d_His', time()+1). '_create_thread_participants_table.php';
+        $messagesTable = date('Y_m_d_His', time()+2). '_create_messages_table.php';
+
+        File::put(database_path('migrations/'. $threadsTable), 'Threads table');
+        File::put(database_path('migrations/'. $threadParticipants), 'Thread participants table');
+        File::put(database_path('migrations/'. $messagesTable), 'Messages table');
+
+        $this->artisan('vendor:publish --tag="laravel-messages-migrations"');
+
+        $migrations = $this->migrations();
+
+        $this->assertCount(3, $migrations);
+
+        $this->assertEquals(
+            "Threads table",
+            file_get_contents(database_path('migrations/'.$threadsTable))
+        );
+
+        $this->assertEquals(
+            "Thread participants table",
+            file_get_contents(database_path('migrations/'.$threadParticipants))
+        );
+
+        $this->assertEquals(
+            "Messages table",
+            file_get_contents(database_path('migrations/'.$messagesTable))
+        );
     }
 
     private function migrations()
